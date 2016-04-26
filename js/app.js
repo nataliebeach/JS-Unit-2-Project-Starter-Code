8a4acf9b27e141f2131b3a4e0a13f79ec27b5eed
Natalie Beach <nataliehbeach@gmail.com>
2016-04-25 21:31:49 -0400

    monday apr 25 class

--- js/app.js
+++ js/app.js
@@ -1,57 +1,100 @@
-/*
-  Please add all Javascript code to this file.
-*/
-
-var $mashable = $("#Mashable");
-var $reddit = $("#Reddit")
+var $mashable = $("#mashable");
+var $reddit = $("#reddit");
+var $digg = $("#digg");
 var $main = $("#main");
+
+//should refactor the following as a single function with parameters for the publication names and URL
+
+// var function getArticles = (publication, url) {
+//   publication.on("click", function(e) {
+//     e.preventDefault();
+//     console.log(publication + "clicked")
+//     $main.hide();
+//     $.ajax ({
+//       type: "GET",
+//       url: url,
+//       success: handleResponse
+//     })
+//   }
+// }
 
 
 $mashable.on("click", function(e){
 	e.preventDefault();
-	console.log("Mashable clicked")
-	//hide all other content
 	$main.hide();
-
-	//api call
 	$.ajax ({
-		type: "GET",
-		url: "http://localhost:3000/mashable.json",
-		success: function(){
-			console.log("mashable api call")
-
-		}
-
-			//show mashable content
-
+  		type: "GET",
+  		url: "http://feedr-api.wdidc.org/mashable.json",
+  		success: function(response){
+        response.hot.forEach( function(i){
+            content.articles.push(
+                {title: i.display_title,
+                image: i.feature_image,
+                category: i.channel,
+                impressions: i.shares.total,
+                url: i.link,
+                description: i.excerpt
+              }
+            )
+        })
+        formatTemplate(content);
+  		}
 	})
 })
 
 $reddit.on("click", function(e){
 	e.preventDefault();
 	console.log("Reddit clicked")
-	//hide all other content
 	$main.hide();
-
-	//api call
 	$.ajax ({
-		type: "GET",
-		url: "https://www.reddit.com/top.json",
-		success: function(){
-			console.log("reddit api call")
-			//create an object with title, image, category, impressions for each article
-			//for each...
+  		type: "GET",
+  		url: "https://www.reddit.com/top.json",
+  		success: function(response) {
+        console.log(response.data)
 
-			//create newObj
+      }
 
-			//testContent[articles].push(newObj)
-		}
 
-	})
+      })
+})
+
+    var handleResponse = function (response) {
+        var allData = response
+        console.log(allData)
+        $main.append(allData)
+    }
+
+        // console.log(testContent);
+        $main.append(testContent);
+
+        //for each...
+
+  			//create newObj
+
+  			//testContent[articles].push(newObj)
+
+
+
+$digg.on("click", function(e){
+  e.preventDefault();
+  console.log("digg clicked")
+  //hide all other content
+  $main.hide();
+
+  $.ajax ({
+    type: "GET",
+    url: "http://feedr-api.wdidc.org/digg.json",
+    success: function(){
+      console.log("digg api call")
+
+    }
+
+  })
 
 
 })
 
+var content = {articles: []}
 
 var testContent = {articles: [
 	{	image: "#",
@@ -68,15 +111,42 @@ var testContent = {articles: [
 	}
 ]}
 
-function formatTemplate(){
+function formatTemplate(data){
 
 	var source = $("#article-template").html()
 
 	var templater = Handlebars.compile(source)
 
-	$main.append(templater(testContent))
+	$main.append(templater(data))
+
+  $main.css("display","inline");
 
 }
 
+function formatPopup(data) {
+  var source = $("#popUp-template").html()
 
+  var templater = Handlebars.compile(source)
+
+  $("#popUp").append(templater(data))
+
+  console.log(templater(data))
+}
+
+//working on Pop up
+
+// // $("body").on("click", ".article", function(e) {
+// //   e.preventDefault();
+// //   formatPopup(content);
+// //   // console.log(formatPopup(content.articles))
+//     // formatPopup($(this).find("h3").text())
+
+
+
+// // })
+// $(".article").on("click", function(){
+//   var data = JSON.parse($(this).attr("content"))
+//   console.log(data)
+//   formatPopup(data)
+// })
 
